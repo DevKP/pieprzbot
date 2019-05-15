ade4a8d4219950f86cb929efde936101313da27d
Ilchenko Vladislav <prodigyvlad@gmail.com>
2019-05-15 16:04:59 +0300

    -

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,3 @@
-Write-Host "Terminating process PersikSharpRelease"
-Try {
-    Invoke-Expression -Command Get-Process | Where-Object { $_.Name -eq "PersikSharpRelease" } | Select-Object -First 1 | Stop-Process
-}
-Catch{
-    Write-Host "Process isn't running!"
-}
 Write-Host "Clearing build folder.."
 Remove-Item "C:\Projects\PersikSharp\Builds\*.*"
 Write-Host "Renaming PersikSharp.exe to PersikSharpRelease.exe..."
@@ -13,7 +6,3 @@ Write-Host "Copying files to build folder..."
 xcopy /y ".\PersikSharp\bin\Release\*.*" "C:\Projects\PersikSharp\Builds\"
 Write-Host "Copying configs..."
 xcopy /y ".\PersikSharp\Configs\*" "C:\Projects\PersikSharp\Builds\Configs\"
-Write-Host "Starting bot!"
-cd  "C:\Projects\PersikSharp\Builds\"
-start .\PersikSharpRelease.exe
-exit 0
