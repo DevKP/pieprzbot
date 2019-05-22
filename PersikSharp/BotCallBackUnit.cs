df5e324b53eb940c981c25d423473fdd0f2fd45b
Klaus Speer <klaus.speer@bk.ru>
2019-05-22 13:44:11 +0000

    space fix

--- PersikSharp/BotCallBackUnit.cs
+++ PersikSharp/BotCallBackUnit.cs
@@ -8,19 +8,19 @@ namespace PersikSharp
     public class BotCallBackUnit
     {
         public int userId;
-		public long chatId;
+        public long chatId;
         public event EventHandler<MessageArgs> callback;
-		
-		public BotCallBackUnit(Message message, EventHandler<MessageArgs> callback)
-		{
-			this.userId   = message.From.Id;
-			this.chatId   = message.Chat.Id;
-			this.callback = callback;
-		}
-		
-		public void InvokeCallback(Message message)
-		{
-			callback?.Invoke(this, new MessageArgs(message));
-		}
+
+        public BotCallBackUnit(Message message, EventHandler<MessageArgs> callback)
+        {
+            this.userId   = message.From.Id;
+            this.chatId   = message.Chat.Id;
+            this.callback = callback;
+        }
+
+        public void InvokeCallback(Message message)
+        {
+            callback?.Invoke(this, new MessageArgs(message));
+        }
     }
 }
