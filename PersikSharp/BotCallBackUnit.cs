7c470003b130d4ddb7245ec0f81df47747173fa4
Klaus Speer <klaus.speer@bk.ru>
2019-05-22 13:32:00 +0000

    New class BotCallBackUnit.

---
+++ PersikSharp/BotCallBackUnit.cs
@@ -0,0 +1,26 @@
+using System;
+using Telegram.Bot.Args;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PersikSharp
+{
+    public class BotCallBackUnit
+    {
+        public int userId;
+		public long chatId;
+        public event EventHandler<MessageArgs> callback;
+		
+		public BotCallBackUnit(Message message, EventHandler<MessageArgs> callback)
+		{
+			this.userId   = message.From.Id;
+			this.chatId   = message.Chat.Id;
+			this.callback = callback;
+		}
+		
+		public void InvokeCallback(Message message)
+		{
+			callback?.Invoke(this, new MessageArgs(message));
+		}
+    }
+}
