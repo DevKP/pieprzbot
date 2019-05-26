2b37f1d1a2db70f0ddc95a8f6da4a1aeebe74cb7
Ilchenko Vladislav <prodigyvlad@gmail.com>
2019-05-26 12:50:35 +0300

    Ability to hide and show console window

---
+++ PersikSharp/Utils/ConsoleWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersikSharp
+{
+    static class ConsoleWindow
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
+    }
+}
