a4d323a4fce674af1ea1411658ef3134f3a32698
Ilchenko Vladislav <prodigyvlad@gmail.com>
2019-05-27 19:33:48 +0300

    Tray icon and close button

--- PersikSharp/Utils/ConsoleWindow.cs
+++ PersikSharp/Utils/ConsoleWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,7 @@ namespace PersikSharp
         public const int SW_HIDE = 0;
         public const int SW_SHOW = 5;
 
+        private static NotifyIcon trayIcon = new NotifyIcon();
 
         public static Task StartTrayAsync()
         {
@@ -28,14 +30,15 @@ namespace PersikSharp
         }
         public static void StartTray()
         {
-            NotifyIcon trayIcon = new NotifyIcon();
             trayIcon.Text = Console.Title;
-            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+            trayIcon.Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
 
             ContextMenu trayMenu = new ContextMenu();
 
             trayMenu.MenuItems.Add("Hide", Min_Click);
             trayMenu.MenuItems.Add("Show", Max_Click);
+            trayMenu.MenuItems.Add("-");
+            trayMenu.MenuItems.Add("Close", Close_Click);
 
             trayIcon.ContextMenu = trayMenu;
             trayIcon.Visible = true;
@@ -50,6 +53,11 @@ namespace PersikSharp
         private static void Max_Click(object sender, EventArgs e)
         {
             ShowWindow(GetConsoleWindow(), SW_SHOW);
+        }
+        private static void Close_Click(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Environment.Exit(0);
         }
     }
 }
