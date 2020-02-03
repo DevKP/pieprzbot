19deec8f2e06dfda28ae91ee507e6e700d70b9dd
0Kitty <prodigyvlad@gmail.com>
2020-02-03 13:20:46 +0200

    deploy fix

--- deploy.ps1
+++ deploy.ps1
@@ -9,7 +9,7 @@ Write-Host "Clearing build folder.."
 Remove-Item "C:\Projects\PersikSharp\Builds\*.exe"
 Remove-Item "C:\Projects\PersikSharp\Builds\*.dll"
 Write-Host "Copying files to build folder..."
-Get-ChildItem .\PerchikSharp\bin\Debug\netcoreapp2.1\ | Copy -Destination C:\Projects\PersikSharp\Builds\ -Recurse -Force
+Get-ChildItem .\PerchikSharp\bin\Debug\netcoreapp3.1\ | Copy -Destination C:\Projects\PersikSharp\Builds\ -Recurse -Force
 Write-Host "Copying configs..."
 xcopy /y ".\PersikSharp\Configs\*" "C:\Projects\PersikSharp\Builds\Configs\"
 xcopy /y ".\PersikSharp\Resources\*" "C:\Projects\PersikSharp\Builds\"
